463894ab59ee2801265fbdba68d68275acbe888a
LaupWing <loc_strict@hotmail.com>
2021-06-03 21:27:04 +0200

    3

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,66 @@
 <template>
    <div class="app">
-      <p>{{name}}</p>
+      <p>{{ name }}</p>
       <button @click="changeName('zelda')">Change Name</button>
    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, reactive, toRefs, ref } from "vue";
+import Job from "./types/Job";
 
 export default defineComponent({
    name: "App",
-   components: {
-   },
-   setup(){
+   components: {},
+   setup() {
       const state = reactive({
-         name: 'Link',
-         age: 25 as string | number
-      })
+         name: "Link",
+         age: 25 as string | number,
+      });
+
+      const jobs = ref<Job[]>([
+         {
+            title: "farm worker",
+            location: "lon lon ranch",
+            salary: 30000,
+            id: "1",
+         },
+         {
+            title: "quarryman",
+            location: "death mountain",
+            salary: 40000,
+            id: "2",
+         },
+         {
+            title: "flute player",
+            location: "the lost woods",
+            salary: 35000,
+            id: "3",
+         },
+         { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+         {
+            title: "prison guard",
+            location: "gerudo valley",
+            salary: 32000,
+            id: "5",
+         },
+      ]);
       return {
-         ...toRefs(state)
-      }
+         ...toRefs(state),
+      };
    },
-   data(){
-      return{
-         name: 'Link'
-      }
+   data() {
+      return {
+         name: "Link",
+      };
    },
-   methods:{
-      changeName(name: string){
-         this.name = name
-      }
-   }
+   methods: {
+      changeName(name: string) {
+         this.name = name;
+      },
+   },
 });
 </script>
 
 <style>
-
 </style>
