1d8f5726a44a8a9f2f8c6f280694166779b5757f
LaupWing <loc_strict@hotmail.com>
2021-12-04 18:17:45 +0100

    update

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,16 +1,18 @@
 <template>
    <div class="jobs-list">
       <ul>
-         <li 
-            v-for="job in jobs"
-            :key="job.id"
-         >
-            <h2> {{job.title}} {{job.location}}</h2>
+         <li v-for="job in jobs" :key="job.id">
+            <h2>{{ job.title }} {{ job.location }}</h2>
             <div class="salary">
-               <p>{{job.salary}} rupees</p>
+               <p>{{ job.salary }} rupees</p>
             </div>
             <div class="description">
-               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus eum eius, nam saepe blanditiis aliquid ab nisi voluptate. Perspiciatis quis porro ad facere minima, obcaecati odit distinctio voluptate quaerat est?</p>
+               <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Temporibus eum eius, nam saepe blanditiis aliquid ab nisi
+                  voluptate. Perspiciatis quis porro ad facere minima, obcaecati
+                  odit distinctio voluptate quaerat est?
+               </p>
             </div>
          </li>
       </ul>
@@ -35,5 +37,33 @@ export default defineComponent({
 </script>
 
 <style scoped>
-   
+.job-list {
+   max-width: 960px;
+   margin: 40px auto;
+}
+.job-list ul {
+   padding: 0;
+}
+.job-list li {
+   list-style-type: none;
+   background: white;
+   padding: 16px;
+   margin: 16px 0;
+   border-radius: 4px;
+}
+.job-list h2 {
+   margin: 0 0 10px;
+   text-transform: capitalize;
+}
+.salary {
+   display: flex;
+}
+.salary img {
+   width: 30px;
+}
+.salary p {
+   color: #17bf66;
+   font-weight: bold;
+   margin: 10px 4px;
+}
 </style>
